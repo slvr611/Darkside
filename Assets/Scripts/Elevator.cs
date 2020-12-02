bd034762cdf4b6857ea65ee6969348576fd62360
matthewjarmusz <mjarmusz@ithaca.edu>
2020-12-02 13:32:31 -0500

    Added sound on elevator (NullReferenceException)
    Currently getting NullReferenceException.  Added code to elevator script and created SoundManagerScript. sound under Assets/Sounds

--- Assets/Scripts/Elevator.cs
+++ Assets/Scripts/Elevator.cs
@@ -6,10 +6,12 @@ public class Elevator : MonoBehaviour
 {
     public float elevatorSpeed = 5;
     public bool isMoving;
+    private SoundManager soundObj;
     // Start is called before the first frame update
     void Start()
     {
         isMoving = false;
+         soundObj = GetComponent<SoundManager>();
     }
 
     // Update is called once per frame
@@ -18,11 +20,13 @@ public class Elevator : MonoBehaviour
         if (isMoving)
         {
             transform.position += transform.up * elevatorSpeed * Time.deltaTime;
+             soundObj.PlayElevator();
         }
     }
 
     public void goUp()
     {
         isMoving = true;
+
     }
 }
---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+   public AudioSource[] mySounds;
+   public AudioSource elevator;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        mySounds = GetComponents<AudioSource>();
+        elevator = mySounds[0];
+    }
+
+    public void PlayElevator() {
+        elevator.Play();
+    }
+
+    }
