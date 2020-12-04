ac8abe79f28d671ba5437016244c85ec8d8d9465
tjones4261 <73116929+tjones4261@users.noreply.github.com>
2020-12-04 14:24:27 -0500

    Built half of a couple levels

---
+++ Assets/Scripts/MoveUpPlatform.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveUpPlatform : MonoBehaviour
+{
+    public float speed = 5; 
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += transform.up * speed * Time.deltaTime;
+    }
+}
