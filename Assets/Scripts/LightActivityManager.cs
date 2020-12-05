1bccb2202f064877dd63b162a67bfe033b0399a6
tjones4261 <73116929+tjones4261@users.noreply.github.com>
2020-12-05 13:51:26 -0500

    Created some scaffolding levels and elevator level

---
+++ Assets/Scripts/LightActivityManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightActivityManager : MonoBehaviour
+{
+    public GameObject[] activateLights;
+    public GameObject[] deactivateLights;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //if (collision.gameObject.CompareTag("Player"))
+        //{
+            for (int i = 0; i < activateLights.Length; i++)
+            {
+                activateLights[i].SetActive(true);
+            }
+
+            for (int h = 0; h < deactivateLights.Length; h++)
+            {
+                deactivateLights[h].SetActive(false);
+            }
+        //}
+    }
+    
+}
