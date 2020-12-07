806166c9e8503c4ee1e0ca6da83fad71638df95f
tjones4261 <73116929+tjones4261@users.noreply.github.com>
2020-12-07 15:27:38 -0500

    Added outline for box sounds

---
+++ Assets/Scripts/boxScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class boxScript : MonoBehaviour
+{
+    public bool isMoving;
+
+    private void Start()
+    {
+        isMoving = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            isMoving = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            isMoving = false;
+        }
+    }
+}
