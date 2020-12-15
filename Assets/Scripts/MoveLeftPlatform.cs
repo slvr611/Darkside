ed08ce054d97c36affe5bfcb0199bc66863e8e4f
tjones4261 <73116929+tjones4261@users.noreply.github.com>
2020-12-15 11:30:21 -0500

    Added level8

---
+++ Assets/Scripts/MoveLeftPlatform.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveLeftPlatform : MonoBehaviour
+{
+    public float speed = 3;
+    public float minXPos = -15.8f;
+    private SoundManager platSound;
+
+    void Start()
+    {
+        platSound = FindObjectOfType<SoundManager>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x > minXPos)
+        {
+            transform.position += -transform.right * speed * Time.deltaTime;
+
+        }
+
+    }
+
+    public void stopPlatform()
+    {
+        speed = 0;
+        platSound.StopPlatform();
+
+    }
+
+    public void resumePlatform()
+    {
+        speed = 1;
+        if (platSound != null)
+        {
+            platSound.PlayPlatform();
+        }
+    }
+}
---
+++ Assets/Scripts/MoveRightPlatform.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveRightPlatform : MonoBehaviour
+{
+    public float speed = 3;
+    public float maxXPos = -15.8f;
+    private SoundManager platSound;
+
+    void Start()
+    {
+        platSound = FindObjectOfType<SoundManager>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x < maxXPos)
+        {
+            transform.position += transform.right * speed * Time.deltaTime;
+
+        }
+
+    }
+
+    public void stopPlatform()
+    {
+        speed = 0;
+        platSound.StopPlatform();
+
+    }
+
+    public void resumePlatform()
+    {
+        speed = 1;
+        if (platSound != null)
+        {
+            platSound.PlayPlatform();
+        }
+    }
+}
